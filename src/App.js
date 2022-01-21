a62c6b5261282abdd915e9b7e692b244e06ac24f
metaverse-kor <jhchoi8899@gmail.com>
2022-01-21 15:32:47 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react"
 import Button from "./components/button";
-import Checkbox from "./components/checkbox";
 import Input from "./components/input";
-import Radio from "./components/radio";
-import Select from "./components/select";
-import Slider from "./components/slider";
 
 const App = () => {
   const [radio, setRadio] = React.useState()
@@ -15,24 +11,16 @@ const App = () => {
 
   return (
     <div>
-      <h1>Theme!!!</h1>
+      <h1>회원가입 신청</h1>
 
-      <h3>Input</h3>
+      <h3>회원 ID</h3>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
       <Input />
 
-      <h3>Checkbox</h3>
-      <Checkbox />
 
-      <h3>Radio</h3>
-      <Radio value="0" checked={radio === "0"} onChange={handleChangeRadio} />
-      <Radio value="1" checked={radio === "1"} onChange={handleChangeRadio} />
-      <Radio value="2" checked={radio === "2"} onChange={handleChangeRadio} />
-
-      <h3>Select</h3>
-      <Select />
-
-      <h3>Slider</h3>
-      <Slider />
 
       <h3>Button</h3>
       <Button>클릭</Button>
