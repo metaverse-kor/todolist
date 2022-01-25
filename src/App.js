49e15b4e3ca6729058bb4696bdca16a986ac3424
metaverse-kor <jhchoi8899@gmail.com>
2022-01-25 16:32:21 +0900

    Changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
         <label htmlFor="">비밀번호 확인</label>
         <Input placeholder="비밀번호를 다시 입력해주세요"/><br/>
         <label htmlFor="">이메일</label>
-        <Input placeholder="이메일 주소를 입력해주세요"/><Select />
+        <Input placeholder="이메일 주소를 입력해주세요"/>@<Select />
         <br/>
       <Button>회원가입</Button>
     </div>
--- src/components/select/index.jsx
+++ src/components/select/index.jsx
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 
 const OPTION = [
     { value: "", name: "직접입력" },
-    { value: "@naver.com", name: "naver.com" },
-    { value: "@daum.net", name: "daum.net" },
-    { value: "@google.com", name: "google.com" },
+    { value: "naver.com", name: "naver.com" },
+    { value: "daum.net", name: "daum.net" },
+    { value: "google.com", name: "google.com" },
+    { value: "yhooo.co.kr", name: "yhooo.co.kr" },
+    { value: "엔타로테사다르", name: "엔타로테사다르.com" },
 ];
 
 
 const SelectBox= () => {
     const [inputValue, setInputValue] = useState('');
-
+    let inputRef = false;
     const changeSelectOptionHandler = (e) => {
         console.log(e.target.value);
         setInputValue(e.target.value);
+        if(e.target.value!==""){
+            console.log('들어옴')
+            inputRef = true;
+            console.log('inputRef =' +inputRef)
+        }
     };
 
     return (
       <>
-      <input type="text" value={inputValue} onChange={changeSelectOptionHandler}/>
+      <input type="text" value={inputValue} onChange={changeSelectOptionHandler} disabled={inputRef}/>
 
       <select onChange={changeSelectOptionHandler} >
           {OPTION.map((option) => (
